425237e0bec267f231dc444bd064595fe0676a7c
Milomir Babic <redant333@gmail.com>
2024-07-29 21:49:46 +0200

    Reorganize files

--- js/loader.js
+++ js/loader.js
@@ -1,11 +1,12 @@
 function createEntryNode(entryJson) {
     const entryTemplate = document.getElementById("id-entryTemplate");
     const entryNode = entryTemplate.content.cloneNode(true);
+    const dataDir = "/data/";
 
     entryNode.querySelector(".data-name").innerText = entryJson.name;
     entryNode.querySelector(".data-location").innerText = entryJson.location;
     entryNode.querySelector(".data-date").innerText = entryJson.date;
-    entryNode.querySelector(".data-thumbnail").src = entryJson.thumbnail;
+    entryNode.querySelector(".data-thumbnail").src = dataDir + entryJson.thumbnail;
 
     return entryNode;
 }
@@ -21,7 +22,7 @@ function displayData(json, destinationId) {
 
 function loadData(jsonPath, destinationId) {
     fetch(jsonPath)
-        .then(data => {return data.json()})
+        .then(data => { return data.json() })
         .then(json => {
             displayData(json, destinationId);
         });
